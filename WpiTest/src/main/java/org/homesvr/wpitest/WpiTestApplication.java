a45770275b60f2b1a5062f66d3c1d9a05998ae11
fishingday <fishingday@gmail.com>
2017-08-25 18:27:38 +0900

    최초 생성

---
+++ WpiTest/src/main/java/org/homesvr/wpitest/WpiTestApplication.java
@@ -0,0 +1,12 @@
+package org.homesvr.wpitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpiTestApplication.class, args);
+	}
+}
